481f2c996d5b97250dd40a0659d7b7f078a09c0e
JCRN <clemons.jason@gmail.com>
2019-07-15 14:51:50 -0700

    completed second stretch goal, added score handler function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 /* eslint-disable no-const-assign */
 //TODO: STEP 1 - Import the useState hook.
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
-
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeScore, setHomeScore] = useState(0);
   const [awayScore, setAwayScore] = useState(0);
+
+  // Stretch goal function to take team name with score and apply to scoreboard after receiving input
+  const handleScoreEvent = () => {
+    // Get scoring team from user
+    let team = prompt("Scoring team: ").trim();
+
+    // Set correct score or reprompt for scoring team
+    switch (team.toLowerCase()) {
+      case "lions":
+      case "home":
+        setHomeScore(homeScore + parseInt(prompt("Points scored: ")));
+        break;
+      case "tigers":
+      case "away":
+        setAwayScore(awayScore + parseInt(prompt("Points scored: ")));
+        break;
+      default:
+        alert(
+          `Please enter a valid team name. For this game: 'Lions', 'Home', 'Tigers', or 'Away'`
+        );
+        handleScoreEvent();
+    }
+  };
+
   return (
     <div className="container">
       <section className="scoreboard">
@@ -31,12 +54,36 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button onClick={() => setHomeScore(homeScore + 7)} className="homeButtons__touchdown">Home Touchdown</button>
-          <button onClick={() => setHomeScore(homeScore + 3)} className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button
+            onClick={() => handleScoreEvent()}
+            // onClick={() => setHomeScore(homeScore + 7)}
+            className="homeButtons__touchdown"
+          >
+            Home Touchdown
+          </button>
+
+          <button
+            onClick={() => setHomeScore(homeScore + 3)}
+            className="homeButtons__fieldGoal"
+          >
+            Home Field Goal
+          </button>
         </div>
         <div className="awayButtons">
-          <button onClick={() => setAwayScore(awayScore + 7)} className="awayButtons__touchdown">Away Touchdown</button>
-          <button onClick={() => setAwayScore(awayScore + 3)} className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button
+            onClick={() => handleScoreEvent()}
+            // onClick={() => setAwayScore(awayScore + 7)}
+            className="awayButtons__touchdown"
+          >
+            Away Touchdown
+          </button>
+
+          <button
+            onClick={() => setAwayScore(awayScore + 3)}
+            className="awayButtons__fieldGoal"
+          >
+            Away Field Goal
+          </button>
         </div>
       </section>
     </div>
